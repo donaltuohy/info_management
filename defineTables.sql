46c0e7c1f4d896f14e64cc62c7a3d54bf9688697
Donal Tuohy <donaltuohy@gmail.com>
2017-12-08 19:53:29 +0000

    Add files via upload

--- defineTables.sql
+++ defineTables.sql
@@ -1,4 +1,5 @@
 --County Table
+
 CREATE TABLE County(
     ID INTEGER not null,
     Name VARCHAR(20) not null,
@@ -6,6 +7,7 @@ CREATE TABLE County(
 );
 
 --Board Member Table
+
 CREATE TABLE Board_Member(
     PPS NUMBER(9,0) not null,
     County_ID INTEGER not null,
@@ -13,11 +15,11 @@ CREATE TABLE Board_Member(
     L_Name VARCHAR(20) not null,
     Phone_no NUMBER(10,0) not null,
     Address VARCHAR2(50) not null,
-    PRIMARY KEY (PPS),
-    CONSTRAINT check_PPS CHECK (PPS < 1000000000)
+    PRIMARY KEY (PPS)
 );
 
 --Division Table
+
 CREATE TABLE Division(
     ID INTEGER not null,
     Name VARCHAR(30) not null,
@@ -26,15 +28,17 @@ CREATE TABLE Division(
 );
 
 --Club Table
+
 CREATE TABLE Club(
     ID INTEGER not null,
     County_ID INTEGER not null,
     Name VARCHAR(20) not null,
     Town VARCHAR(20) not null,
     PRIMARY KEY (ID)
-)
+);
 
 --Team Table
+
 CREATE TABLE Team(
     ID INTEGER not null,
     Division_ID INTEGER not null,
@@ -51,6 +55,7 @@ CREATE TABLE Team(
 );
 
 --Pitch Table
+
 CREATE TABLE Pitch(
     ID INTEGER not null,
     Name VARCHAR(20) not null,
@@ -59,10 +64,11 @@ CREATE TABLE Pitch(
 );
 
 --Match Table
+
 CREATE TABLE Match(
     ID INTEGER not null,
     Match_date DATE not null,
-    Match_time VARCHAR(5) not null,  --"16:20" --"23.00"
+    Match_time VARCHAR(5) not null,
     Team_1_ID INTEGER not null,
     Team_2_ID INTEGER not null,
     Pitch_ID INTEGER not null,
@@ -72,24 +78,26 @@ CREATE TABLE Match(
 );
 
 --Manager Table
+
 CREATE TABLE Manager(
     ID INTEGER not null,
-    Name VARCHAR(20) not null,
+	F_Name VARCHAR(20) not null,
+    L_Name VARCHAR(20) not null,
     Address VARCHAR(50),
     Phone_no INTEGER,
-    PRIMARY KEY (PPS),
-    CONSTRAINT check_PPS CHECK (PPS < 1000000000)
+    PRIMARY KEY (ID)
 );
 
 --Player Table
+
 CREATE TABLE Player(
     PPS NUMBER(9,0) not null,
     F_Name VARCHAR(20) not null,
     L_Name VARCHAR(20) not null,
+	Team_ID INTEGER not null,
     Phone_no NUMBER(10,0),
     Address VARCHAR(50),
-    PRIMARY KEY (PPS),
-    CONSTRAINT check_PPS CHECK (PPS < 1000000000)
+    PRIMARY KEY (PPS)
 );
 
 
--- insertFiles.sql
+++ insertFiles.sql
@@ -1,83 +1,86 @@
 --County(County ID, Name)
-INSERT INTO County Values(1, 'Wicklow')
-INSERT INTO County Values(2, 'Dublin')
-INSERT INTO County Values(3, 'Kildare')
-INSERT INTO County Values(4, 'Carlow')
-INSERT INTO County Values(5, 'Cork')
-INSERT INTO County Values(6, 'Meath')
-INSERT INTO County Values(7, 'Mayo')
+INSERT INTO County Values(1, 'Wicklow');
+INSERT INTO County Values(2, 'Dublin');
+INSERT INTO County Values(3, 'Kildare');
+INSERT INTO County Values(4, 'Carlow');
+INSERT INTO County Values(5, 'Cork');
+INSERT INTO County Values(6, 'Meath');
+INSERT INTO County Values(7, 'Mayo');
 
 --Board_Member(PPS Number, County ID, Name, Phone Number, Address)
-INSERT INTO Board_Member Values(100000000, 1, 'John', 'Smith', 0871234567, '1 main road, Kilmac, Wicklow')
-INSERT INTO Board_Member Values(100000001, 2, 'April', 'Long', 0871234267, '10 left road, Dalkey, Dublin')
-INSERT INTO Board_Member Values(100000002, 3, 'Dan', 'Tilley', 0871524567, '63 Dark lane, Leixslip, Kildare')
-INSERT INTO Board_Member Values(100000003, 4, 'Paul', 'Freehy', 0871238267, 'Number 6, Borris, Carlow')
-INSERT INTO Board_Member Values(100000004, 5, 'Andrew', 'Byrne', 0861234567, 'Howardshore, Ballyhea, Cork')
-INSERT INTO Board_Member Values(100000005, 6, 'Damien',  'Rice', 0871234907, 'Damiens Castle, Trim, Meath')
-INSERT INTO Board_Member Values(100000006, 7, 'Dermot',  'Kennedy', 0871239007, 'Eoins Den, Westport, Mayo')
+INSERT INTO Board_Member Values(100000000, 1, 'John', 'Smith', 0871234567, '1 main road, Kilmac, Wicklow');
+INSERT INTO Board_Member Values(100000001, 2, 'April', 'Long', 0871234267, '10 left road, Dalkey, Dublin');
+INSERT INTO Board_Member Values(100000002, 3, 'Dan', 'Tilley', 0871524567, '63 Dark lane, Leixslip, Kildare');
+INSERT INTO Board_Member Values(100000003, 4, 'Paul', 'Freehy', 0871238267, 'Number 6, Borris, Carlow');
+INSERT INTO Board_Member Values(100000004, 5, 'Andrew', 'Byrne', 0861234567, 'Howardshore, Ballyhea, Cork');
+INSERT INTO Board_Member Values(100000005, 6, 'Damien',  'Rice', 0871234907, 'Damiens Castle, Trim, Meath');
+INSERT INTO Board_Member Values(100000006, 7, 'Dermot',  'Kennedy', 0871239007, 'Eoins Den, Westport, Mayo');
+
 
 --Division(ID, Name, County ID)
-INSERT INTO Division Values(1, 'Senior', 1)
-INSERT INTO Division Values(2, 'Intermediate', 1)
-INSERT INTO Division Values(3, 'Junior', 1)
-INSERT INTO Division Values(4, 'Senior', 2)
-INSERT INTO Division Values(5, 'Senior', 3)
-INSERT INTO Division Values(6, 'Intermediate', 3)
+INSERT INTO Division Values(1, 'Senior', 1);
+INSERT INTO Division Values(2, 'Intermediate', 1);
+INSERT INTO Division Values(3, 'Junior', 1);
+INSERT INTO Division Values(4, 'Senior', 2);
+INSERT INTO Division Values(5, 'Senior', 3);
+INSERT INTO Division Values(6, 'Intermediate', 3);
 
 --Club(ID, County ID, Name, Town)
-INSERT INTO Team Values(1, 1, 'Rathnew GAA', 'Rathnew')
-INSERT INTO Team Values(2, 1, 'St. Patricks GAA', 'Wicklow Town')
-INSERT INTO Team Values(3, 1, 'Kilmacanogue GAA', 'Kilmacanogue')
-INSERT INTO Team Values(4, 1, 'Kiltegan GAA', 'Kiltegan')
-INSERT INTO Team Values(5, 1, 'Laragh GAA', 'Laragh')
-INSERT INTO Team Values(6, 1, 'Kilcoole GAA', 'Kilcoole')
-INSERT INTO Team Values(7, 2, 'St. Vincents GAA', 'Marino')
-INSERT INTO Team Values(8, 2, 'Cuala GAA', 'Dalkey')
+INSERT INTO Club Values(1, 1, 'Rathnew GAA', 'Rathnew');
+INSERT INTO Club Values(2, 1, 'St. Patricks GAA', 'Wicklow Town');
+INSERT INTO Club Values(3, 1, 'Kilmacanogue GAA', 'Kilmacanogue');
+INSERT INTO Club Values(4, 1, 'Kiltegan GAA', 'Kiltegan');
+INSERT INTO Club Values(5, 1, 'Laragh GAA', 'Laragh');
+INSERT INTO Club Values(6, 1, 'Kilcoole GAA', 'Kilcoole');
+INSERT INTO Club Values(7, 2, 'St. Vincents GAA', 'Marino');
+INSERT INTO Club Values(8, 2, 'Cuala GAA', 'Dalkey');
 
 
---Team(ID, Division ID, Club ID, Manager ID, Points, Won, Drawn, Lost, Score_For, Score_Against)
-INSERT INTO Team Values(1, 1, 1, 4, 0, 0, 0, 0, 0, 0)
-INSERT INTO Team Values(2, 1, 2, 2, 0, 0, 0, 0, 0, 0)
-INSERT INTO Team Values(3, 1, 3, 9, 0, 0, 0, 0, 0, 0)
-INSERT INTO Team Values(4, 2, 3, 1, 0, 0, 0, 0, 0, 0)
-INSERT INTO Team Values(5, 2, 4, 5, 0, 0, 0, 0, 0, 0)
-INSERT INTO Team Values(6, 3, 5, 8, 0, 0, 0, 0, 0, 0)
-INSERT INTO Team Values(7, 3, 6, 6, 0, 0, 0, 0, 0, 0)
-INSERT INTO Team Values(8, 4, 7, 7, 0, 0, 0, 0, 0, 0)
-INSERT INTO Team Values(9, 4, 8, 3, 0, 0, 0, 0, 0, 0)
+--Team(ID, Division ID, Club ID, Manager ID, Captain_PPS Points, Won, Drawn, Lost, Score_For, Score_Against)
+INSERT INTO Team Values(1, 1, 1, 4, 600100100, 0, 0, 0, 0, 0, 0);
+INSERT INTO Team Values(2, 1, 2, 2, 700100100, 0, 0, 0, 0, 0, 0);
+INSERT INTO Team Values(3, 1, 3, 9, 100100100, 0, 0, 0, 0, 0, 0);
+INSERT INTO Team Values(4, 2, 3, 1, 200100100, 0, 0, 0, 0, 0, 0);
+INSERT INTO Team Values(5, 2, 4, 5, 500100100, 0, 0, 0, 0, 0, 0);
+INSERT INTO Team Values(6, 3, 5, 8, 300100100, 0, 0, 0, 0, 0, 0);
+INSERT INTO Team Values(7, 3, 6, 6, 400100100, 0, 0, 0, 0, 0, 0);
+INSERT INTO Team Values(8, 4, 7, 7, 800100100, 0, 0, 0, 0, 0, 0);
+INSERT INTO Team Values(9, 4, 8, 3, 900100100, 0, 0, 0, 0, 0, 0);
 
 --Pitch(ID, Name, Address)
-INSERT INTO Pitch Values(1, 'Fitzimons Park', 'Quill Road, Kilmacanogue, Wicklow')
-INSERT INTO Pitch Values(2, 'Parc Ui Greystones', 'Killcarrig, Greystones, Wicklow')
-INSERT INTO Pitch Values(3, 'Caraguna Stadium', 'Blackrock GAA Grounds, Dublin')
-INSERT INTO Pitch Values(4, 'Cork Pitch', 'Charleville, Cork')
-INSERT INTO Pitch Values(5, 'Wesport Stadium', 'Quayside, Westport, Mayo')
-INSERT INTO Pitch Values(6, 'Coolkenno Park', 'Coolkenno, Wicklow')
+INSERT INTO Pitch Values(1, 'Fitzimons Park', 'Quill Road, Kilmacanogue, Wicklow');
+INSERT INTO Pitch Values(2, 'Parc Ui Greystones', 'Killcarrig, Greystones, Wicklow');
+INSERT INTO Pitch Values(3, 'Caraguna Stadium', 'Blackrock GAA Grounds, Dublin');
+INSERT INTO Pitch Values(4, 'Cork Pitch', 'Charleville, Cork');
+INSERT INTO Pitch Values(5, 'Wesport Stadium', 'Quayside, Westport, Mayo');
+INSERT INTO Pitch Values(6, 'Coolkenno Park', 'Coolkenno, Wicklow');
 
 --Match(ID, Match Date, Match Time, First Team ID, Second Team ID, Pitch ID)
-INSERT INTO match Values(1, '01/09/17', "16:00", 3, 2, 2, 0, 0)
-INSERT INTO match Values(2, '12/09/17', "14:00", 3, 1, 1, 0, 0)
-INSERT INTO match Values(3, '25/10/17', "12:00", 4, 3, 6, 0, 0)
-INSERT INTO match Values(4, '03/08/17', "11:00", 6, 7, 2, 0, 0)
-INSERT INTO match Values(5, '10/09/17', "10:00", 7, 6, 1, 0, 0)
-INSERT INTO match Values(6, '10/09/17', "13:00", 9, 8, 3, 0, 0)
+INSERT INTO match Values(1, TO_DATE('01/09/17', 'DD/MM/YY'), '16:00', 3, 2, 2, 0, 0);
+INSERT INTO match Values(2, TO_DATE('12/09/17', 'DD/MM/YY'), '14:00', 3, 1, 1, 0, 0);
+INSERT INTO match Values(3, TO_DATE('25/10/17', 'DD/MM/YY'), '12:00', 4, 3, 6, 0, 0);
+INSERT INTO match Values(4, TO_DATE('03/08/17', 'DD/MM/YY'), '11:00', 6, 7, 2, 0, 0);
+INSERT INTO match Values(5, TO_DATE('10/09/17', 'DD/MM/YY'), '10:00', 7, 6, 1, 0, 0);
+INSERT INTO match Values(6, TO_DATE('10/09/17', 'DD/MM/YY'), '13:00', 9, 8, 3, 0, 0);
 
 --Manager(Manager ID, Name, Address, Phone_no)
-INSERT INTO Manager Values(1, 'Gerry', 'Tuohy', 'Ardmore Heights, Bray, Wicklow', 0876573050)
-INSERT INTO Manager Values(2, 'Paul', 'Daly', 'Blessigton Road, Wicklow', 0865435555)
-INSERT INTO Manager Values(3, 'Niall', 'King', 'Main Road, Dublin', 0876543212)
-INSERT INTO Manager Values(4, 'John', 'Kennedy', 'Not main Road, Tallaght, Dublin', 0876432121)
-INSERT INTO Manager Values(5, 'Eoin', 'Rea', 'Hollywood, Wicklow', 0823412022)
-INSERT INTO Manager Values(6, 'Edward' , 'Rafferty', 'Avalon, Westport, Wicklow', 0884325566)
-INSERT INTO Manager Values(7, 'Nicola', 'Sturgeon', 'Edingburgh Road, Westport, Wicklow', 0841231231)
-INSERT INTO Manager Values(8, 'Kenny',  'Koldrick', '1 Dalkey Road, Dalkey, Dublin')
-INSERT INTO Manager Values(9, 'Ste', 'Long', '2nd place, Charleville, Cork', 0848889299)
+INSERT INTO Manager Values(1, 'Gerry', 'Tuohy', 'Ardmore Heights, Bray, Wicklow', 0876573050);
+INSERT INTO Manager Values(2, 'Paul', 'Daly', 'Blessigton Road, Wicklow', 0865435555);
+INSERT INTO Manager Values(3, 'Niall', 'King', 'Main Road, Dublin', 0876543212);
+INSERT INTO Manager Values(4, 'John', 'Kennedy', 'Not main Road, Tallaght, Dublin', 0876432121);
+INSERT INTO Manager Values(5, 'Eoin', 'Rea', 'Hollywood, Wicklow', 0823412022);
+INSERT INTO Manager Values(6, 'Edward' , 'Rafferty', 'Avalon, Westport, Wicklow', 0884325566);
+INSERT INTO Manager Values(7, 'Nicola', 'Sturgeon', 'Edingburgh Road, Westport, Wicklow', 0841231231);
+INSERT INTO Manager Values(8, 'Kenny',  'Koldrick', '1 Dalkey Road, Dalkey, Dublin', 0876671199);
+INSERT INTO Manager Values(9, 'Ste', 'Long', '2nd place, Charleville, Cork', 0848889299);
 
 --Player(PPS NUMBER, Name, Team_ID, Phone number, Address)
-INSERT INTO Player Values(100100100, 'Donal Tuohy', 3, 0876672989, 'Killough Upper, Kilmacanogue, Wicklow')
-INSERT INTO Player Values(200100100, 'Eoin Tuohy', 3, 08721493322, 'Killough Upper, Kilmacanogue, Wicklow')
-INSERT INTO Player Values(300100100, 'Marcus Rafferty', 3, 0870870871, 'Kilbarrack Road, Kilmacanogue, Wicklow')
-INSERT INTO Player Values(400100100, 'Cian Rafferty', 3, 0856676234, 'Kilbarrack Road, Kilmacanogue, Wicklow')
-INSERT INTO Player Values(500100100, 'Marc Lennon', 4, 0853421212, '1 Main Street, Rathnew, Wicklow')
-INSERT INTO Player Values(600100100, 'John Star', 4, 0843128429, '2 Main Street, Rathnew, Wicklow')
-INSERT INTO Player Values(700100100, 'Gerry McCartney', 4, 0838254682, '3 Main Street, Rathnew, Wicklow')
+INSERT INTO Player Values(100100100, 'Donal', 'Tuohy', 3, 0876672989, 'Killough Upper, Kilmacanogue, Wicklow');
+INSERT INTO Player Values(200100100, 'Eoin', 'Tuohy', 4, 08721493322, 'Killough Upper, Kilmacanogue, Wicklow');
+INSERT INTO Player Values(300100100, 'Marcus', 'Rafferty', 6, 0870870871, 'Kilbarrack Road, Kilmacanogue, Wicklow');
+INSERT INTO Player Values(400100100, 'Cian', 'Rafferty', 7, 0856676234, 'Kilbarrack Road, Kilmacanogue, Wicklow');
+INSERT INTO Player Values(500100100, 'Marc', 'Lennon', 5, 0853421212, '1 Main Street, Rathnew, Wicklow');
+INSERT INTO Player Values(600100100, 'John', 'Star', 1, 0843128429, '2 Main Street, Rathnew, Wicklow');
+INSERT INTO Player Values(700100100, 'Gerry', 'McCartney', 2, 0838254682, '3 Main Street, Rathnew, Wicklow');
+INSERT INTO Player Values(800100100, 'Gerry', 'Paul', 8, 0838252082, '5 Main Street, Rathnew, Wicklow');
+INSERT INTO Player Values(900100100, 'Ger', 'Lister', 9, 0838254322, '8 Main Street, Rathnew, Wicklow');
